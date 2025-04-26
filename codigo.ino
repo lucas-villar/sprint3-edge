7e554cad29af086ad58ea320acf46124ea0c9af2
Lucas Villar <163485070+lucas-villar@users.noreply.github.com>
2025-04-26 06:43:54 -0300

    Update codigo.ino

--- codigo.ino
+++ codigo.ino
@@ -1,16 +1,13 @@
-
-// Bibliotecas
+// Bibiliotecas
 #include<ArduinoJson.h>
 #include<DHT.h>
 #include<LiquidCrystal_I2C.h>
-
 // Pinos
 #define DHTPIN 4
 #define DHTTYPE 22
-#define ledR 2
-#define ledG 3 
+#define ledR 3
+#define ledG 2
 #define buzzerPin 5
-
 // Variáveis Globais
 DHT dht(DHTPIN, DHTTYPE);
 LiquidCrystal_I2C lcd(0x27, 20, 4);
@@ -22,6 +19,7 @@ int temp = 0;
 int umid = 0;
 bool tempAlert = false;
 bool umidAlert = false;
+
 
 void checkSensor() {
   temp = dht.readTemperature();
@@ -46,36 +44,36 @@ void setup() {
 void loop() {
   lcd.clear();
 
-  // Título
+  //Título
   lcd.setCursor(0, 0);
   lcd.print(" Monitoramento UTI ");
 
-  // Temperatura
+  //Temperatura
   lcd.setCursor(0, 1);
   lcd.print(" Temp: ");
   lcd.print(temp);
   lcd.print((char)223);  // Símbolo de grau
   lcd.print("C");
-  if (temp > MAX_TEMP){
+  if (temp > MAX_TEMP) {
     lcd.print("  ALTO!");
     tempAlert = true;
-  } else if (temp < MIN_TEMP){
+  } else if (temp < MIN_TEMP) {
     lcd.print("  BAIXO!");
     tempAlert = true;
   } else {
     lcd.print("   OK!");
     tempAlert = false;
   }
-  
-  // Umidade
+
+  //Umidade
   lcd.setCursor(0, 2);
   lcd.print(" Umid: ");
   lcd.print(umid, 1);
   lcd.print("%");
-  if (umid > MAX_UMID){
+  if (umid > MAX_UMID) {
     lcd.print("   ALTO!");
     umidAlert = true;
-  } else if (umid < MIN_UMID){
+  } else if (umid < MIN_UMID) {
     lcd.print("   BAIXO!");
     umidAlert = true;
   } else {
@@ -83,27 +81,27 @@ void loop() {
     umidAlert = false;
   }
 
-  // Alertas
+  //Alertas
   lcd.setCursor(0, 3);
-  if (tempAlert == true || umidAlert == true){
+  if (tempAlert == true || umidAlert == true) {
     lcd.print("       ALERTA       ");
-    for (int a=0; a<2; a++){
-      for (int b=0; b<2; b++){
+    for (int a = 0; a < 2; a++) {
+      for (int b = 0; b < 2; b++) {
         tone(buzzerPin, 1000);
-        delay(150);            
+        delay(150);
         noTone(buzzerPin);
         delay(150);
       }
       digitalWrite(ledG, LOW);
       digitalWrite(ledR, HIGH);
       delay(1500);
-    }  
+    }
   } else {
     lcd.print("       NORMAL       ");
     digitalWrite(ledR, LOW);
     digitalWrite(ledG, HIGH);
     delay(3600);
   }
-  
+
   checkSensor();
 }
